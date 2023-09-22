bcf8d4a21579510e928adda32df1a3d1946a2bc3
ZhengYingchao <1092350982@qq.com>
2023-09-22 15:14:01 +0800

    Plot格式

--- q2/preprocess.py
+++ q2/preprocess.py
@@ -6,11 +6,48 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
 
-df = pd.read_csv("data/origin/表2-患者影像信息血肿及水肿的体积及位置.csv")
+df = pd.read_csv("data/水肿体积时序.csv")
 df_time = pd.read_csv("data/origin/time.csv")
 
+print(df.head())
+print(df.info())
 
-print(df_time.head())
-print(df_time.info())
+cols = []
 
+# 获取流水号字段
+for c in df.columns:
+    if c.find("流水号") != -1:
+        cols.append(c)
+        # df[c]=df[c].astype(float).astype(int)
+
+
+print(cols)
+
+for index in df.index:
+    for col in cols:
+
+        # 获取流水号
+        num = df.loc[index,col]
+        # 获取时间字段
+        time_col = col.replace('流水号','时间点')
+        # 获取对应时间
+        time = df_time.loc[df_time[col] == num, time_col]
+        # df.loc[index,time_col]=time
+
+        # 查找它所在的行
+        target_row = df_time[df_time.eq(num).any(axis=1)]
+        # 如果找到目标行，获取该行的索引
+        if not target_row.empty:
+            row_index = target_row.index[0]
+            time_column_index = df_time.columns.get_loc(col)
+            # 获取同一行的前一列的值
+            if row_index >= 0:
+                time = df_time.iloc[row_index, time_column_index - 1]
+                print(time)
+        else:
+            print(f"未找到目标值{row_index} {col}")
+
+
+
+print(df.head())
 
---
+++ q2/test.py
@@ -0,0 +1,3 @@
+col='随访1时间'
+
+print(col.replace('时间', '流水号'))
